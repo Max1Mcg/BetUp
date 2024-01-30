9c65253284408d340f933a862468c548466c2cd5
Arenchick <Arenchik1@yandex.ru>
2024-01-30 22:51:05 +0300

    Test: Метод получения свойства у Json по заданному пути

--- BetUp/Controllers/OddsController.cs
+++ BetUp/Controllers/OddsController.cs
@@ -7,6 +7,7 @@ using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 using System.Text.Json;
 using System;
 using BetUp.Models;
+using BetUp.Helpers;
 
 namespace WebApiBetsBot.Controllers
 {
@@ -37,6 +38,15 @@ namespace WebApiBetsBot.Controllers
             //sharedClient.DefaultRequestHeaders.Add("Cookie", "_egb_session=eERMbXJHZEhFZDZrdGk4dWNvZ0kwZnNZY0cyTUJFRTJlSTVVOGhSaTVEcW9kVTdVVE1KZDduUllNdUFOYmpxc3Y3VlFrQldpMjFlYisxNUhSaXZPeHJiLytYWUdwMzRFbGZMbWN1T0NYMWJmdWdUMEpwaVhRZndSajRDYUJ5cHM1UnZPakplSEpOZyt4M1JUdy96M1RnODhHWmY2eDR1R3hRWlM2c0tmNmp6ZnVha3dOc2VPQ2xMeEcwVWhac3ZOVVJuS3lnT0NtTTZaNlcvcGtaV090THRNM2dQUjRLTElRblVhNGZQM1h2MUc3T0hZZ3hNaG5aSU9ScGljTitkc0NDYlJrOHNmalZjOTdkaWIzYzVzbXc5M3dMeUJaN3NVYjlqM1hxOXczMCtzOENlbmk0R1A1RUVlMFdGZWtGT2hCTTZieEN1S2xMeVArQXE1M1Voc1RBPT0tLTk0YVdpd0IycnZsZEJPYkhwM2tCUUE9PQ%3D%3D--2cf0b1535e943054a31310ff1eaa4e5f46fa163b; cloudflare_uid=MoEbA1oyg--Kfy32CRSeYx-2QaPSbFyyGN2azc4j; is_first_time=1; referer=");
             //sharedClient.DefaultRequestHeaders.Add("Connection", "keep-alive");
             return await sharedClient.GetAsync("baseAddress").Result.EnsureSuccessStatusCode().Content.ReadAsStringAsync();
+        }
+
+        [Route("[action]")]
+        [HttpGet]
+        public IActionResult TestGetMethod()
+        {
+            JsonHelper jsonHelper = new JsonHelper();
+            var value = jsonHelper.GetPropertyValue<string>("address.geo.lng");
+            return Ok(value);
         }
 
         [Route("[action]")]
---
+++ BetUp/Helpers/JsonHelper.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace BetUp.Helpers
+{
+    public class JsonHelper
+    {
+        private string testJson = @"
+        {
+            ""id"": 1,
+            ""name"": ""Leanne Graham"",
+            ""username"": ""Bret"",
+            ""email"": ""Sincere@april.biz"",
+            ""address"": {
+                  ""street"": ""Kulas Light"",
+                  ""suite"": ""Apt. 556"",
+                  ""city"": ""Gwenborough"",
+                  ""zipcode"": ""92998-3874"",
+                  ""geo"": {
+                    ""lat"": ""-37.3159"",
+                    ""lng"": ""81.1496""
+                  }
+            }
+        }";
+
+
+        public T GetPropertyValue<T>(string path) where T : class
+        {
+            JObject jsonObject = JObject.Parse(testJson);
+            var jToken = jsonObject.First.Root;
+
+            var resultJToken = GetJTokenByPath(jToken, path);
+
+            return resultJToken.Value<T>();
+        }
+
+        private JToken GetJTokenByPath(JToken jToken, string path)
+        {
+            var propertyName = path.Split('.').FirstOrDefault();
+            if (string.IsNullOrEmpty(propertyName)) return jToken;
+
+            var value = jToken.SelectToken(propertyName);
+            if (value == null) return jToken;
+
+            if (propertyName == path) return value;
+
+            return GetJTokenByPath(value, path.Substring(propertyName.Length+1));
+        }
+    }
+}
